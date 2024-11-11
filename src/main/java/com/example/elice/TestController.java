9e23d86d46eb596416dd476eb9a6519f635750dd
EC2 Default User <ec2-user@ip-172-31-7-129.ap-northeast-2.compute.internal>
2024-11-11 04:47:25 +0000

    안녕 클라우 4기

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice!";
+    return "Hello Elice! this is from sb coach with elice awesome cloud forth";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
